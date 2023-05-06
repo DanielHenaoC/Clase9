c12508dbe7636288d014f7fd79672a0ec2672cc9
Daniel Henao <henaocardenas843@gmail.com>
2023-05-05 19:26:26 -0500

    Crea un formulario para ingresar el usuario y los dos botones para registrar y limpiar
    see: #4

--- ventana1.py
+++ ventana1.py
@@ -1,7 +1,8 @@
 import sys
 
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QLabel, QHBoxLayout, QApplication
+from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QLabel, QHBoxLayout, QApplication, QFormLayout, QLineEdit, \
+    QPushButton
 from PyQt5 import QtGui
 
 class Ventana1(QMainWindow):
@@ -13,8 +14,8 @@ class Ventana1(QMainWindow):
 
         self.setWindowIcon(QtGui.QIcon("imagen/pngwing.com (1).png"))
 
-        self.ancho = 1200
-        self.alto = 800
+        self.ancho = 900
+        self.alto = 600
 
         self.resize(self.ancho, self.alto)
 
@@ -42,10 +43,118 @@ class Ventana1(QMainWindow):
 
         self.horizontal.setContentsMargins(30, 30, 30, 30)
 
+        # Layout izquierdo
+
+        # Creamos el layout izquierdo
+        self.ladoizquierdo = QFormLayout()
+
+        self.letrero1 = QLabel()
+
+        self.letrero1.setText("Información del cliente")
+
+        self.letrero1.setFont(QFont("Andale Mono", 20))
+
+        self.letrero1.setStyleSheet("color: #000080")
+
+        self.ladoizquierdo.addRow(self.letrero1)
+
+        self.letrero2 = QLabel()
+
+        self.letrero2.setFixedWidth(340)
+
+        self.letrero2.setText("Por favor ingrese la información de cliente "
+                              "\nen el formulario de abajo. los campos marcados"
+                              "\ncon asterisco son obligatorios.")
+
+        self.letrero2.setFont(QFont("Andale Mono", 10))
+
+        self.letrero2.setStyleSheet("color: #000080; margin-bottom: 40px;"
+                                    "margin-top: 20px;"
+                                    "padding-bottom: 10px;"
+                                    "border: 2px solid #000080;"
+                                    "border-left: none;"
+                                    "border-right: none;"
+                                    "border-top: none;")
+
+        self.ladoizquierdo.addRow(self.letrero2)
+
+        self.nombreCompleto = QLineEdit()
+        self.nombreCompleto.setFixedWidth(220)
+
+        self.ladoizquierdo.addRow("Nombre Completo*", self.nombreCompleto)
+
+        self.usuario = QLineEdit()
+        self.usuario.setFixedWidth(220)
+
+        self.ladoizquierdo.addRow("Usuario*", self.usuario)
+
+        self.password = QLineEdit()
+        self.password.setFixedWidth(220)
+        self.password.setEchoMode(QLineEdit.Password)
+
+        self.ladoizquierdo.addRow("Password*", self.password)
+
+        self.password2 = QLineEdit()
+        self.password2.setFixedWidth(220)
+        self.password2.setEchoMode(QLineEdit.Password)
+
+        self.ladoizquierdo.addRow("Password*", self.password2)
+
+        self.documento = QLineEdit()
+        self.documento.setFixedWidth(220)
+
+        self.ladoizquierdo.addRow("Documento*", self.documento)
+
+        self.correo = QLineEdit()
+        self.correo.setFixedWidth(220)
+
+        self.ladoizquierdo.addRow("Correo*", self.correo)
+
+        self.botonRegistrar = QPushButton("Registrar")
+
+        self.botonRegistrar.setFixedWidth(90)
+
+        self.botonRegistrar.setStyleSheet("background-color: #008B45;"
+                                          "color: #FFFFFF;"
+                                          "padding: 10px;"
+                                          "margin-top: 40px;")
+
+        self.botonRegistrar.clicked.connect(self.accion_botonRegistrar)
+
+        self.botonLimpiar = QPushButton("Limpiar")
+
+        self.botonLimpiar.setFixedWidth(90)
+
+        self.botonLimpiar.setStyleSheet("background-color: #008B45;"
+                                          "color: #FFFFFF;"
+                                          "padding: 10px;"
+                                          "margin-top: 40px;")
+
+        self.botonLimpiar.clicked.connect(self.accion_botonLimpiar)
+
+        self.ladoizquierdo.addRow(self.botonRegistrar, self.botonLimpiar)
+
+        self.horizontal.addLayout(self.ladoizquierdo)
+
 
 
         # ------------- SIEMPRE PONER DE ULTIMO ---------------------
         self.fondo.setLayout(self.horizontal)
+
+
+
+
+    # Metodo del botonLimpiar:
+    def accion_botonLimpiar(self):
+        pass
+
+    # Metodo del botonRegistrar
+    def accion_botonRegistrar(self):
+        pass
+
+
+
+
 
 
 if __name__ == '__main__':
