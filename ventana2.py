f4d6157a911e285e837d1667f9d1781970646078
Daniel Henao <henaocardenas843@gmail.com>
2023-05-18 10:22:08 -0500

    Creamos la ventana que muestra un scroll con los usuarios identificados con un boton que tiene su cedula, y se muestra en forma de items de a 3 por fila
    see: #11

--- ventana2.py
+++ ventana2.py
@@ -1,8 +1,12 @@
+import math
 import sys
 
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QLabel, QHBoxLayout, QVBoxLayout, QApplication
+from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QLabel, QHBoxLayout, QVBoxLayout, QApplication, QScrollArea, \
+    QWidget, QGridLayout, QButtonGroup, QPushButton
 from PyQt5 import QtGui
+
+from cliente import Cliente
 
 
 class Ventana2(QMainWindow):
@@ -30,13 +34,93 @@ class Ventana2(QMainWindow):
         self.setCentralWidget(self.fondo)
         self.vertical = QVBoxLayout()
 
+        self.letrero1 = QLabel()
+        self.letrero1.setText("Información del cliente")
+        self.letrero1.setFont(QFont("Andale Mono", 20))
+        self.letrero1.setStyleSheet("color: #000080")
+        self.vertical.addWidget(self.letrero1)
+        self.vertical.addStretch()
 
+        # Creamos un scroll
+        self.scrollArea = QScrollArea()
+        self.scrollArea.setStyleSheet("background-color: transparent;")
+        self.scrollArea.setWidgetResizable(True)
 
+        self.contenedor = QWidget()
+        self.cuadricula = QGridLayout(self.contenedor)
+        self.scrollArea.setWidget(self.contenedor)
+        self.vertical.addWidget(self.scrollArea)
 
+        self.file = open('datos/clientes.txt', 'rb')
+        self.usuario = []
+
+        while self.file:
+            linea = self.file.readline().decode('UTF-8')
+            lista = linea.split(";")
+            if linea == '':
+                break
+
+            u = Cliente(
+                lista[0],
+                lista[1],
+                lista[2],
+                lista[3],
+                lista[4],
+                lista[5],
+                lista[6],
+                lista[7],
+                lista[8],
+                lista[9],
+                lista[10],
+            )
+
+            self.usuario.append(u)
+        self.file.close()
+
+        self.numeroUsuario = len(self.usuario)
+        self.contador = 0
+        self.elementosPorColumna = 3
+        self.filas = math.ceil(self.numeroUsuario/self.elementosPorColumna) + 1
+
+        # El boton para todos los botones
+        self.botones = QButtonGroup()
+        self.botones.setExclusive(False)
+
+        for fila in range(1, self.numeroUsuario):
+            for columna in range(1, self.elementosPorColumna+1):
+                if self.contador < self.numeroUsuario:
+                    self.ventanaAuxiliar = QWidget()
+                    self.ventanaAuxiliar.setFixedHeight(100)
+                    self.ventanaAuxiliar.setFixedWidth(200)
+
+                    self.verticalCuadricula = QVBoxLayout()
+                    self.botonAccion = QPushButton(self.usuario[self.contador].documento)
+                    self.botonAccion.setFixedWidth(150)
+                    self.botonAccion.setStyleSheet("background-color: #008B45; color: black; padding: 10px;")
+                    self.verticalCuadricula.addWidget(self.botonAccion)
+                    self.botones.addButton(self.botonAccion, int(self.usuario[self.contador].documento))
+                    self.verticalCuadricula.addStretch()
+                    self.ventanaAuxiliar.setLayout(self.verticalCuadricula)
+                    self.cuadricula.addWidget(self.ventanaAuxiliar, fila, columna)
+                    self.contador += 1
+
+        self.botones.idClicked.connect(self.metodo_accionBotones)
 
 
         # OJO PONER AL FINAL
         self.fondo.setLayout(self.vertical)
+
+
+
+
+    def metodo_accionBotones(self,cedulaUsuario):
+        print(cedulaUsuario)
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
